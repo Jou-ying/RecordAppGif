976689d73ad2a195161183ded072cfd8324bee3b
ZoeLi <zoeli@tstartel.com>
2020-05-08 16:54:34 +0800

    ChangeColor

--- RecordAppGif/ViewController.swift
+++ RecordAppGif/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var backGroundView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func ChangeBackGround(_ sender: Any) {
+        
+        self.backGroundView.backgroundColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha:  CGFloat.random(in: 0...1))
+    }
+    
 
 }
 
